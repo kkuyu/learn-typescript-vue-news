bf2a72c756c3c6b671a6f985f7047ff74a0c47b3
kkuyu <devils67@naver.com>
2022-03-26 21:47:24 +0900

    API 함수 타입 정의, 프롭스 속성 타입 정의

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -25,20 +25,22 @@
   </ul>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { NewsItem } from '@/api';
+import Vue, { PropType } from 'vue'
+export default Vue.extend({
   props: {
     items: {
-      type: Array,
+      type: Array as PropType<NewsItem[]>,
       required: true,
     },
   },
   computed: {
-    // listItems() {
-    //   return this.$store.getters.fetchedList;
-    // }
-  }
-}
+    listItems(): any {
+      return this.$store.getters.fetchedList;
+    }
+  },
+})
 </script>
 
 <style scoped>
