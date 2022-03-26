7e1370cd4b278a85e0db8e2838c80acc1d6a8edf
kkuyu <devils67@naver.com>
2022-03-26 21:34:17 +0900

    API 파일에 타입스크립트 적용 및 호출 로직 구현

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -5,12 +5,22 @@
 </template>
 
 <script>
+import { fetchNews } from '@/api';
 import ListItem from '../components/ListItem.vue';
 
 export default {
   components: {
     ListItem
   },
+  methods: {
+    async fetchNewsItems() {
+      const response = await fetchNews();
+      console.log(response.data);
+    },
+  },
+  created () {
+    this.fetchNewsItems();
+  }
 }
 </script>
 
