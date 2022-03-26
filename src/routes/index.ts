70192833cf06b25d32d463c39de2721a504f6c59
kkuyu <devils67@naver.com>
2022-03-26 21:16:57 +0900

    라우터 파일에 TS 적용

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
+import VueRouter, { NavigationGuardNext, Route } from 'vue-router';
 import { ItemView, UserView } from '../views';
 import createListView from '../views/CreateListView';
 import bus from '../utils/bus';
@@ -18,7 +18,11 @@ export default new VueRouter({
       path: '/news',
       name: 'news',
       component: createListView('NewsView'),
-      beforeEnter(routeTo, routeFrom, next) {
+      beforeEnter(
+        routeTo: Route,
+        routeFrom: Route,
+        next: NavigationGuardNext<Vue>
+      ) {
         bus.$emit('on:progress');
         store.dispatch('FETCH_LIST', routeTo.name)
           .then(() => next())
@@ -29,7 +33,11 @@ export default new VueRouter({
       path: '/ask',
       name: 'ask',
       component: createListView('AskView'),
-      beforeEnter(routeTo, routeFrom, next) {
+      beforeEnter(
+        routeTo: Route,
+        routeFrom: Route,
+        next: NavigationGuardNext<Vue>
+      ) {
         bus.$emit('on:progress');
         store.dispatch('FETCH_LIST', routeTo.name)
           .then(() => next())
@@ -40,7 +48,11 @@ export default new VueRouter({
       path: '/jobs',
       name: 'jobs',
       component: createListView('JobsView'),
-      beforeEnter(routeTo, routeFrom, next) {
+      beforeEnter(
+        routeTo: Route,
+        routeFrom: Route,
+        next: NavigationGuardNext<Vue>
+      ) {
         bus.$emit('on:progress');
         store.dispatch('FETCH_LIST', routeTo.name)
           .then(() => next())
@@ -50,23 +62,31 @@ export default new VueRouter({
     {
       path: '/item/:id',
       component: ItemView,
-      beforeEnter(routeTo, routeFrom, next) {
+      beforeEnter(
+        routeTo: Route,
+        routeFrom: Route,
+        next: NavigationGuardNext<Vue>
+      ) {
         bus.$emit('on:progress');
         const itemId = routeTo.params.id;
         store.dispatch('FETCH_ITEM', itemId)
           .then(() => next())
-          .catch(err => new Error('failed to fetch item details', err));
+          .catch(err => new Error('failed to fetch item details'));
       },
     },
     {
       path: '/user/:id',
       component: UserView,
-      beforeEnter(routeTo, routeFrom, next) {
+      beforeEnter(
+        routeTo: Route,
+        routeFrom: Route,
+        next: NavigationGuardNext<Vue>
+      ) {
         bus.$emit('on:progress');
         const itemId = routeTo.params.id;
         store.dispatch('FETCH_USER', itemId)
           .then(() => next())
-          .catch(err => new Error('failed to fetch user profile', err));
+          .catch(err => new Error('failed to fetch user profile'));
       },
     }
   ]
